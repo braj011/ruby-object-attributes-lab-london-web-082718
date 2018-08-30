0fd1a29cf094f230f87a9403ec97f5c70db05d8a
Bhagya Raj <bhagyaraj01@gmail.com>
2018-08-30 08:54:42 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+def Person 
+  
+  def name             # reader method
+    @name
+  end
+ 
+  def name=(new_name)   # writer method
+    @name = new_name
+  end
+  
+  def breed           # reader method
+    @breed
+  end
+ 
+  def breed=(new_breed)   # writer method
+    @breed = new_breed
+  end
+  
+  
+end
