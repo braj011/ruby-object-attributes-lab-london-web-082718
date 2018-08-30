f26a1f63dbb1404001747eba042fe14959f1ff74
Bhagya Raj <bhagyaraj01@gmail.com>
2018-08-30 08:52:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,10 +4,21 @@ class Dog
 #     @name = name
 #   end
  
-  def name
+  def name   
     @name
   end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
   
+  def breed   
+    @breed
+  end
+ 
+  def breed=(new_name)
+    @name = new_name
+  end
   
   
 end
